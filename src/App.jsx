f5e072998a4599006c8a18769087fbd417c0d41c
noushinizar <noushinizar8129@gmail.com>
2024-10-22 11:57:43 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Navbar } from "./components/Navbar"
 import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
 import { Home } from "./pages/Home"
+import { AuthContextProvider } from "./Context/AuthContext"
 function App() {
   return (
     <>
+    <AuthContextProvider>
      <Navbar/> 
      <Routes>
       <Route path="/" element={<Home/>} />
      </Routes>
+    </AuthContextProvider>
     </>
   )
 }
