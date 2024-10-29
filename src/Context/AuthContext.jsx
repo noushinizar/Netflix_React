2528fa5f5af95d04fa04c5f5d8572eaa96a10038
noushinizar <noushinizar8129@gmail.com>
2024-10-29 16:54:33 +0530

    ci

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -7,12 +7,16 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { setDoc,doc} from 'firebase/firestore'
+
 const AuthContext = createContext();
 export function AuthContextProvider({ children }) {
  const [user,setUser] = useState({})
 
   function signUp(email,password){
-    return createUserWithEmailAndPassword(auth,email,password)
+    createUserWithEmailAndPassword(auth,email,password);
+    setDoc(doc(db,'users',email),{
+      savedShows:[]
+    })
   }
   function logIn(email,password){
     return signInWithEmailAndPassword(auth,email,password)
